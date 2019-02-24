07b497c90593280ee32c55faba0fbd9e6f912ece
eric <niyo1eric@gmail.com>
2019-02-24 14:21:12 +0200

    rails movie review

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+  
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+         has_many :movies
+         has_many :reviews, dependent: :destroy
+end
---
+++ db/migrate/20190223164250_ad_movie_id_to_reviews.rb
@@ -0,0 +1,4 @@
+class AdMovieIdToReviews < ActiveRecord::Migration[5.1]
+  def change
+  end
+end
